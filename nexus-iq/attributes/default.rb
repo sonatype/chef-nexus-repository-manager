8ddb33150472d138b8168ff6e273e4da31beecf5
Christian Fernau <cfernau@sonatype.com>
2017-09-08 20:45:57 +0700

    INT-350_create_installation_recipes

---
+++ nexus-iq/attributes/default.rb
@@ -0,0 +1,5 @@
+default['java']['jdk_version'] = '8'
+default['java']['install_flavor'] = 'oracle'
+default['java']['oracle']['accept_oracle_download_terms'] = true
+
+default['nexus-iq']['version'] = '1.35.0-02'
