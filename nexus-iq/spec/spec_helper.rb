b53979adbac193588a087640994a9151919a0a3b
Christian Fernau <cfernau@sonatype.com>
2017-09-18 22:41:45 +0700

    remove rspec tests

--- nexus-iq/spec/spec_helper.rb
+++
@@ -1,8 +0,0 @@
-#
-# Cookbook:: nexus-iq
-#
-# Copyright:: Copyright (c) 2017-present Sonatype, Inc. Apache License, Version 2.0.
-
-# frozen_string_literal: true
-require 'chefspec'
-require 'chefspec/berkshelf'
