1b42dc82ef31fe16145d9e2794316a8c442d0c54
bigspotteddog <bink.lynch@gmail.com>
2017-09-18 22:16:23 -0600

    Revert "Initial commit of nexus3. Still need to rename folders and all nexus3/nxrm references to their proper names."
    This reverts commit 7664f7b26203217fc9cdf6e76c8f88889c809fe4.

--- nexus3/spec/spec_helper.rb
+++
@@ -1,8 +0,0 @@
-#
-# Cookbook:: nexus3
-#
-# Copyright:: Copyright (c) 2017-present Sonatype, Inc. All rights reserved.
-
-# frozen_string_literal: true
-require 'chefspec'
-require 'chefspec/berkshelf'
