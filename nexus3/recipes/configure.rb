42155e5b0886dd34ba765c7e44db20293fd8a911
bigspotteddog <bink.lynch@gmail.com>
2017-09-21 10:06:30 -0600

    Removed the unused template folder.

--- nexus3/recipes/configure.rb
+++ nexus3/recipes/configure.rb
@@ -3,12 +3,3 @@
 # Recipe:: configure
 #
 # Copyright:: Copyright (c) 2017-present Sonatype, Inc. All rights reserved.
-
-start_script = node['nexus3']['sonatype_dir'] + '/start-nexus3.sh'
-
-template start_script do
-  source 'start-nexus3.sh.erb'
-  owner 'root'
-  group 'root'
-  mode '0755'
-end
