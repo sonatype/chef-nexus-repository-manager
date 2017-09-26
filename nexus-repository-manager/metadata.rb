39394f4030626e6856e347448efac259c3f7e1e0
bigspotteddog <bink.lynch@gmail.com>
2017-09-26 13:44:32 -0700

    Added platforms and descriptions to metadata.

--- nexus-repository-manager/metadata.rb
+++ nexus-repository-manager/metadata.rb
@@ -11,8 +11,13 @@ description 'Installs/Configures Nexus Repository Manager'
 long_description 'Installs/Configures Nexus Repository Manager'
 version '0.1.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
-issues_url 'https://github.com/sonatype/nexus-chef-cookbook/issues'
-source_url 'https://github.com/sonatype/nexus-chef-cookbook'
+issues_url 'https://github.com/sonatype/chef-nexus-cookbook/issues'
+source_url 'https://github.com/sonatype/chef-nexus-cookbook'
+supports 'ubuntu', '>= 16.04'
+supports 'centos', '>= 7.3'
+
+recipe 'nexus-repository-manager::default', 'Installs Nexus Repository Manager and starts it as systemd service.'
+recipe 'nexus-repository-manager::docker', 'Installs Nexus Repository Manager. Instead of a systemd service start-nexus-repository-manager.sh is provided in install_dir.'
 
 depends 'java', '~> 1.50'
 depends 'tar', '~> 2.1'
