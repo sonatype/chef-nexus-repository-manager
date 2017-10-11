68077b75875defaf50e4c7f07ebc661dd0739d55
bigspotteddog <bink.lynch@gmail.com>
2017-10-11 16:25:13 -0700

    Split the hazelcast default configuration to its own file. Flip the hazelcast enabled properties. Added hazelcast.xml.erb header.

--- attributes/default.rb
+++ attributes/default.rb
@@ -24,37 +24,6 @@ default['nexus_repository_manager']['nexus']['data']['etc']['path'] = default['n
 default['nexus_repository_manager']['nexus']['data']['log']['path'] = default['nexus_repository_manager']['nexus']['data']['path'] + '/log'
 default['nexus_repository_manager']['nexus']['data']['tmp']['path'] = default['nexus_repository_manager']['nexus']['data']['path'] + '/tmp'
 
-# hazelcast configuration
-## group configuration
-default['nexus_repository_manager']['hazelcast']['group']['name'] = 'nexus'
-default['nexus_repository_manager']['hazelcast']['group']['password'] = 'nexus123'
-
-## multicast configuration
-default['nexus_repository_manager']['hazelcast']['network']['join']['multicast']['enabled'] = true
-default['nexus_repository_manager']['hazelcast']['network']['join']['multicast']['multicast_group'] = '224.2.2.3'
-default['nexus_repository_manager']['hazelcast']['network']['join']['multicast']['multicast_port'] = 54327
-
-## tcp-ip configuration
-default['nexus_repository_manager']['hazelcast']['network']['join']['tcp_ip']['enabled'] = false
-default['nexus_repository_manager']['hazelcast']['network']['join']['tcp_ip']['interface'] = '127.0.0.1'
-default['nexus_repository_manager']['hazelcast']['network']['join']['tcp_ip']['members'] = ['127.0.0.1']
-
-## aws configuration
-default['nexus_repository_manager']['hazelcast']['network']['join']['aws']['enabled'] = false
-default['nexus_repository_manager']['hazelcast']['network']['join']['aws']['iam_role'] = ''
-default['nexus_repository_manager']['hazelcast']['network']['join']['aws']['region'] = 'us-east-1'
-default['nexus_repository_manager']['hazelcast']['network']['join']['aws']['tag_key'] = 'Platform'
-default['nexus_repository_manager']['hazelcast']['network']['join']['aws']['tag_value'] = 'nexus-repository-manager'
-
-## hazelcast discovery configuration
-default['nexus_repository_manager']['properties']['hazelcast']['discovery']['enabled'] = false
-default['nexus_repository_manager']['hazelcast']['network']['join']['hazelcast_discovery']['iam_role'] = ''
-default['nexus_repository_manager']['hazelcast']['network']['join']['hazelcast_discovery']['region'] = 'us-east-1'
-default['nexus_repository_manager']['hazelcast']['network']['join']['hazelcast_discovery']['tag_key'] = 'Platform'
-default['nexus_repository_manager']['hazelcast']['network']['join']['hazelcast_discovery']['security_group_name'] = ''
-default['nexus_repository_manager']['hazelcast']['network']['join']['hazelcast_discovery']['tag_value'] = 'nexus-repository-manager'
-default['nexus_repository_manager']['hazelcast']['network']['join']['hazelcast_discovery']['hz_port'] = 5701
-
 # nexus.properties configuration
 default['nexus_repository_manager']['nexus_properties']['application_port'] = '8081'
 default['nexus_repository_manager']['nexus_properties']['application_host'] = '0.0.0.0'
