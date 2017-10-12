e0078a5ffb555d6c8646ec50bd7d54e3d80cb973
Christian Fernau <cfernau@sonatype.com>
2017-10-13 00:07:58 +0700

    INT-techdebt-add-aws-support

--- recipes/aws.rb
+++ recipes/aws.rb
@@ -6,6 +6,20 @@
 
 include_recipe 'poise-python'
 
+region = node['nexus_repository_manager']['aws']['region']
+
+Chef.event_handler do
+  on :run_failed do |exception|
+    system('/opt/cloudformation/signal-completion.sh false')
+  end
+end
+
+Chef.event_handler do
+  on :run_completed do
+    system('/opt/cloudformation/signal-completion.sh true')
+  end
+end
+
 directory '/opt/cloudformation/' do
   owner 'root'
   group 'root'
@@ -84,6 +98,25 @@ end
 
 include_recipe 'nexus_repository_manager::default'
 
-execute 'signal_completion' do
-  command '/opt/cloudformation/signal-completion.sh &'
+nexus_log = node['nexus_repository_manager']['nexus']['data']['log']['path'] + '/nexus.log'
+
+bash 'wait for nexus startup' do
+  code <<-EOH
+    until grep -m 1 "Started Sonatype Nexus" #{nexus_log} ; do
+      systemctl is-active nexus-repository-manager.service || exit 1
+      sleep 1
+    done
+  EOH
+end
+
+bash 'check instance health' do
+  code <<-EOH
+    instance_id=`cat /var/lib/cloud/data/instance-id`
+    load_balancer=`aws elb describe-load-balancers --region #{region} | python -c "import json,sys;describeLoadBalancersResult=json.load(sys.stdin);print [loadBalancerDescription['LoadBalancerName'] for loadBalancerDescription in describeLoadBalancersResult['LoadBalancerDescriptions'] if(any(instance['InstanceId'] == '$instance_id' for instance in loadBalancerDescription['Instances']))][0]"`
+    instance_state=`aws elb describe-instance-health --region #{region} --load-balancer-name ${load_balancer} --instances ${instance_id} --query InstanceStates[0].State --output text`
+    echo load balancer state is ${instance_state} lb: {load_balancer} instance: ${instance_id}
+    test ${instance_state} == "InService" || exit 1
+  EOH
+  retries 300
+  retry_delay 2
 end
