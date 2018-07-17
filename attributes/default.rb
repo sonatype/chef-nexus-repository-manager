26e4c76bab505f49983505c5974675e9d31fd540
Sonatype <sonatype-ci@sonatype.com>
2018-07-17 18:58:41 +0000

    Update Repository Manager to 3.12.1-01.

--- attributes/default.rb
+++ attributes/default.rb
@@ -10,9 +10,9 @@ default['java']['jdk']['8']['x86_64']['checksum'] = '0b26c7fcfad20029e6e0989e678
 default['java']['oracle']['accept_oracle_download_terms'] = true
 
 # nexus configuration
-default['nexus_repository_manager']['version'] = '3.9.0-01'
+default['nexus_repository_manager']['version'] = '3.12.1-01'
 default['nexus_repository_manager']['nexus_download_url'] = "https://download.sonatype.com/nexus/3/nexus-#{node['nexus_repository_manager']['version']}-unix.tar.gz"
-default['nexus_repository_manager']['nexus_download_sha256'] = '9989a455bda4c62032b10b98c2a1ac40b3be78feca90edf4f714a91da08f24ac'
+default['nexus_repository_manager']['nexus_download_sha256'] = '6eb09810767ed1bfe33c995a2fa550b667189aa0ed99f002ce826436f60765ef'
 default['nexus_repository_manager']['sonatype']['path'] = '/opt/sonatype'
 default['nexus_repository_manager']['nexus_data']['path'] = '/nexus-data'
 default['nexus_repository_manager']['license_s3_bucket'] = nil
